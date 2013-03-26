e141dc873548ddeee48505806d17ae33b1c67ce6
Garth Colasurdo <garth.colasurdo@gmail.com>
2013-03-26 15:33:23 -0600

    Volunteer Sign Up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Volunteer::Application.config.secret_token = 'db9cdb71ac457f37ade3a6060ac0d43e00fbebdf1f0098e721542a23ef0a61989557cac72657c75564c412a84c8e327681fd6e64be80ba42e9111b7849818bb5'
